c5377072b0dff1773cc4924093230b729530dc09
carlos <caaz160782@gmail.com>
2024-07-25 21:33:45 -0600

    add message error

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,20 +1,46 @@
-import { useState } from 'react';
-import { categories } from "./data/categories"
+import { useState ,ChangeEvent,FormEvent} from 'react';
+import type { DraftExpense, Value } from '../types';
+import { categories } from "../data/categories"
+import ErrorMessage from './ErrorMessage';
 import DatePicker from 'react-date-picker';
 import 'react-date-picker/dist/DatePicker.css';
 import 'react-calendar/dist/Calendar.css';
 
 
-type ValuePiece = Date | null;
-
-type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const ExpenseForm = () => {
-  const [value, onChange] = useState<Value>(new Date());
+  const[error, setError]=useState('')
+  const [expense, setExpense]=useState<DraftExpense>({
+    amount:0,
+    expenseName:'',
+    category:'',
+    date:new Date()
+  })
+
+  const handleChange =(e:ChangeEvent<HTMLInputElement>|ChangeEvent<HTMLSelectElement>)=>{
+    const {name,value} =e.target;
+    const isAmountField=['amount'].includes(name)
+    setExpense({...expense,
+         [name] : isAmountField ? Number(value):value
+      })
+  }
+  const handleChangeDate=(value:Value)=>{
+    setExpense({...expense,
+      date:value})
+  }
+
+  const handleSubmit =(e:FormEvent<HTMLFormElement>)=>{
+      e.preventDefault()
+      if(Object.values(expense).includes('')){
+        setError('Todos los campos son obligatorios');
+        return
+      }
+  }
 
   return (
-    <form className='space-y-5'>
+    <form className='space-y-5' onSubmit={handleSubmit}>
        <legend className='uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2 '>Nuevo Gasto</legend>
+       {error && <ErrorMessage>{error}</ErrorMessage>}
        <div className='flex flex-col gap-2'>
         <label
         htmlFor='expenseName'
@@ -26,6 +52,8 @@ const ExpenseForm = () => {
           placeholder='Añade el nombre del gasto'
           className='bg-slate-100 p-2'
           name='expenseName'
+          value={expense.expenseName}
+          onChange={handleChange}
         />
        </div>
        <div className='flex flex-col gap-2'>
@@ -39,6 +67,8 @@ const ExpenseForm = () => {
           placeholder='Añade la cantidad del gasto'
           className='bg-slate-100 p-2'
           name='amount'
+          value={expense.amount}
+          onChange={handleChange}
         />
        </div>
        <div className='flex flex-col gap-2'>
@@ -50,6 +80,8 @@ const ExpenseForm = () => {
           id="category"         
           className='bg-slate-100 p-2'
           name='category'
+          value={expense.category}
+          onChange={handleChange}
         >
           <option value="">Seleccione</option>
           {
@@ -68,8 +100,8 @@ const ExpenseForm = () => {
         >Fecha gasto: </label>
        <DatePicker 
         className={"bg-slate-100 p-2 border-0"}
-         onChange={onChange} 
-         value={value} />
+        onChange={handleChangeDate}
+        value={expense.date} />
        </div>
        <input
         type="submit"
